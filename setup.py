1268bda01f30d329d347f2364af53834e75568b0
Aggrey <39681748+Emmanuel-Aggrey@users.noreply.github.com>
2024-11-26 19:21:38 +0000

    Initial commit with setup.py for packaging

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_django_app',          # The name of your package
+    name='ktg_storage',            # The name of your app
     version='0.1.0',               # Version of your app
     packages=find_packages(),      # Automatically find packages in the app directory
     include_package_data=True,     # Include additional files specified in MANIFEST.in
-    install_requires=[             # Dependencies your app needs
+    install_requires=[             # List dependencies your app needs
         'django>=3.0',
     ],
-    # Short description of your app
-    description='A reusable Django app for XYZ functionality',
+    description='A reusable Django app for managing storage functionality',
     # Long description from README file
     long_description=open('README.md').read(),
-    # Specify markdown format for README
-    long_description_content_type='text/markdown',
+    long_description_content_type='text/markdown',  # Markdown format for README
     author='Your Name',
     author_email='your.email@example.com',
-    url='https://github.com/yourusername/my_django_app',  # URL for your project
-    classifiers=[  # Classifiers for PyPI (optional, but recommended)
+    url='https://github.com/KayakTech/ktg_storage',  # URL for your GitHub repo
+    classifiers=[  # Classifiers for Python packages
         'Framework :: Django',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
